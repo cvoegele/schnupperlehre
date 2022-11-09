81f66bbde60566fe1b73c90d3926415a9477581a
Christoph Vögele <christoph.voegele@fhnw.ch>
2022-11-09 22:35:38 +0100

    add coconut hit

--- js/lib.js
+++ js/lib.js
@@ -27,4 +27,27 @@ class GameObject {
     draw() {
         image(this.img, this.x, this.y, this.width, this.height);
     }
+
+    touch(other) {
+        var rect1 = this;
+        var x1 = other.x, y1 = other.y, x2 = x1 + other.width, y2 = y1 + other.height;
+        if (rect1.x > x1) {
+            x1 = rect1.x;
+        }
+        if (rect1.y > y1) {
+            y1 = rect1.y;
+        }
+        if (rect1.x + rect1.width < x2) {
+            x2 = rect1.x + rect1.width;
+        }
+        if (rect1.y + rect1.height < y2) {
+            y2 = rect1.y + rect1.height;
+        }
+        return (x2 <= x1 || y2 <= y1) ? false : {x: x1, y: y1, width: x2 - x1, height: y2 - y1};
+    }
+
+}
+
+function random(min, max) {
+    return Math.random() * (max - min) + min;
 }
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-
 let backGroundImage = new GameObject(0, 0, 1000, 1000, "../assets/beach.webp")
 let player = new GameObject(0, 0, 100, 100, "../assets/trout.png")
-
+let coconut = new GameObject(0, 0, 50, 50, "../assets/coconut.png")
+let hitCount = 0;
 
 function setup() {
     //createCanvas just makes a canvas, for you to draw into. You can customize the size.
@@ -11,9 +11,12 @@ function setup() {
     player.x = 100;
     player.y = 500;
 
+    coconut.x = random(0, 800);
+
     // all gameobjects need to be setup before they can be drawn
     player.setup()
     backGroundImage.setup()
+    coconut.setup()
 }
 
 function draw() {
@@ -25,8 +28,19 @@ function draw() {
         player.move(10, 0)
     }
 
+    coconut.move(0, 2)
+
+    if (coconut.touch(player)) {
+        console.log("Player has touched the coconut.")
+        hitCount++;
+        coconut.y = 0;
+        coconut.x = random(0, 800);
+    }
+
+    //need to be drawn in order of visibility
     backGroundImage.draw()
-    player.draw();
+    player.draw()
+    coconut.draw()
 }
 
 function windowResized() {
