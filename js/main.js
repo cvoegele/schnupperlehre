6ca358022678591155ef39c45bba9ee7b343df9d
Christoph Vögele <christoph.voegele@fhnw.ch>
2022-11-09 23:55:53 +0100

    final solution

--- js/main.js
+++ js/main.js
@@ -1,22 +1,16 @@
 let backGroundImage = new GameObject(0, 0, 1000, 1000, "../assets/beach.webp")
 let player = new GameObject(0, 0, 100, 100, "../assets/trout.png")
 let coconut = new GameObject(0, 0, 50, 50, "../assets/coconut.png")
-let hitCount = 0;
+var hitcount = 0
 
 function setup() {
-    //createCanvas just makes a canvas, for you to draw into. You can customize the size.
     createCanvas(windowWidth, windowHeight);
-
-    //initialize position of gameobjects at the start of the game
-    player.x = 100;
-    player.y = 500;
-
-    coconut.x = random(0, 800);
-
-    // all gameobjects need to be setup before they can be drawn
-    player.setup()
     backGroundImage.setup()
+    player.setup()
     coconut.setup()
+
+    player.x = 300
+    player.y = 500
 }
 
 function draw() {
@@ -27,27 +21,25 @@ function draw() {
         player.moveBy(10, 0)
     }
 
-    coconut.moveBy(0, 2)
-
-    if (coconut.touch(player)) {
-        console.log("Player has touched the coconut.")
-        hitCount++;
-        coconut.y = 0;
-        coconut.x = random(0, 800);
+    if(coconut.touch(player)){
+        coconut.y = 0
+        coconut.x = random(0,800)
+        hitcount++
     }
 
     if (coconut.y > 1000) {
-        coconut.y = 0;
+        coconut.y = 0
     }
 
-    //need to be drawn in order of visibility
+    coconut.moveBy(0, 2)
+
     backGroundImage.draw()
     player.draw()
     coconut.draw()
 
     textSize(32)
     fill(50);
-    text("Punkte " + hitCount, 10, 10, 200, 100);
+    text("Punkte " + hitcount, 10, 10, 200, 100);
 }
 
 function windowResized() {
